aa461af839efaa9bf7b54e51b86459bb9e48aac4
Thomas abruti <ton.email@example.com>
2025-07-09 16:46:16 +0200

    ajout de seeder

--- apwap/app/Models/User.php
+++ apwap/app/Models/User.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use HasUuids;
+    use HasFactory;
 
     protected $table = 'users';
 
